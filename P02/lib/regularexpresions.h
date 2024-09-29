e9dd6ece878f30de65f1223d787a7e0cd80b9d21
Lucas <alu0101550145@ull.edu.es>
2024-09-29 16:04:16 +0000

    REVO

---
+++ P02/lib/regularexpresions.h
@@ -0,0 +1,13 @@
+#ifndef REGULAREXPRESIONS_H
+#define REGULAREXPRESIONS_H
+
+#include <regex>
+
+class RegularExpresion {
+ public:
+
+ private:
+
+};
+
+#endif //REGULAREXPRESIONS_H
---
+++ P02/src/regularexpresions.cc
@@ -0,0 +1,1 @@
+#include "../lib/regularexpresions.h"
